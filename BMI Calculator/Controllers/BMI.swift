a5e71648d5cd970ba170864364f1bce2f3fb08db
Sardorbek Saydamatov <sardorbeksaydamatov@Sardorbeks-MacBook-Air-2.local>
2022-11-10 20:17:52 +0500

    first commit

---
+++ BMI Calculator/Controllers/BMI.swift
@@ -0,0 +1,13 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Sardorbek Saydamatov on 09/11/22.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+import UIKit
+
+struct BMI {
+    let value:Float
+    let advise:String
+    let color:UIColor
+}
